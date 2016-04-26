a6ee96e65cc354117fff2119ed3f4875841e6e8e
agoldner <agoldner@ucsd.edu>
2016-04-25 20:58:14 -0700

    Complete track from lab to UTC.

--- app/src/main/java/com/example/agoldner/maps/MapsActivity.java
+++ app/src/main/java/com/example/agoldner/maps/MapsActivity.java
@@ -46,15 +46,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
 
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude())).title("updated path"));
+                mMap.clear();
+                LatLng newPos = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPos, 15));
+                mMap.addMarker(new MarkerOptions().position(newPos).title("Current Movement"));
+
             }
 
             @Override
